ede220046ab0c59f7d48c465b9efbacb972a6c12
LogicHou <logichou@qq.com>
2022-08-17 14:38:39 +0800

    add const.go

---
+++ internal/pkg/const/const.go
@@ -0,0 +1,28 @@
+package cst
+
+// Exchange Names
+const (
+	BINANCE = "binance" //Binance https://www.binance-cn.com
+	OKEX    = "okex"    // OKEx SPOT https://www.okex.me/spot/trade
+)
+
+// Market Types
+const (
+	MARKET_TYPE_TRADE      = "trade"
+	MARKET_TYPE_ORDERBOOK  = "orderbook"
+	MARKET_TYPE_KLINE      = "kline"
+	MARKET_TYPE_KLINE_3M   = "kline_3m"
+	MARKET_TYPE_KLINE_5M   = "kline_5m"
+	MARKET_TYPE_KLINE_15M  = "kline_15m"
+	MARKET_TYPE_KLINE_30M  = "kline_30m"
+	MARKET_TYPE_KLINE_1H   = "kline_1h"
+	MARKET_TYPE_KLINE_3H   = "kline_3h"
+	MARKET_TYPE_KLINE_6H   = "kline_6h"
+	MARKET_TYPE_KLINE_12H  = "kline_12h"
+	MARKET_TYPE_KLINE_1D   = "kline_1d"
+	MARKET_TYPE_KLINE_3D   = "kline_3d"
+	MARKET_TYPE_KLINE_1W   = "kline_1w"
+	MARKET_TYPE_KLINE_15D  = "kline_15d"
+	MARKET_TYPE_KLINE_1MON = "kline_1mon"
+	MARKET_TYPE_KLINE_1Y   = "kline_1y"
+)
